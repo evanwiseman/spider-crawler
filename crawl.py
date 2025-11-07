c48b261ca9093abb3f20977498d5bf5959af630f
evanwiseman <evanwiseman2000@gmail.com>
2025-11-07 11:25:01 -0800

    add h1 and p parse

--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from urllib import parse
 
 
@@ -6,3 +7,24 @@ def normalize_url(url: str) -> str:
     joined = result.netloc + result.path
     normalized = joined.rstrip("/").lower()
     return normalized
+
+
+def get_h1_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    h1 = soup.find(name="h1")
+    if h1 is None:
+        return ""
+    return h1.get_text()
+
+
+def get_first_paragraph_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    main = soup.find(name="main")
+
+    # high priority main and paragraph
+    if main and main.p:
+        return main.p.get_text()
+
+    # low priority outside of main
+    p = soup.find(name="p")
+    return p.get_text() if p else ""
--- test_crawl.py
+++ test_crawl.py
@@ -1,30 +1,132 @@
 import unittest
-from crawl import normalize_url
+from crawl import normalize_url, get_h1_from_html, get_first_paragraph_from_html
 
 
-class TestCrawl(unittest.TestCase):
-    def test_normalize_url_protocol(self):
+class TestNormalizeURL(unittest.TestCase):
+    def test_default(self):
         input_url = "https://blog.boot.dev/path"
         actual = normalize_url(input_url)
         expected = "blog.boot.dev/path"
         self.assertEqual(actual, expected)
 
-    def test_normalize_url_slash(self):
+    def test_slash(self):
         input_url = "https://blog.boot.dev/path/"
         actual = normalize_url(input_url)
         expected = "blog.boot.dev/path"
         self.assertEqual(actual, expected)
 
-    def test_normalize_url_capitals(self):
+    def test_capitals(self):
         input_url = "https://BLOG.boot.dev/path"
         actual = normalize_url(input_url)
         expected = "blog.boot.dev/path"
         self.assertEqual(actual, expected)
 
-    def test_normalize_url_http(self):
+    def test_http(self):
         input_url = "http://BLOG.boot.dev/path"
         actual = normalize_url(input_url)
         expected = "blog.boot.dev/path"
+        self.assertEqual(actual, expected)
+
+
+class TestGetH1FromHTML(unittest.TestCase):
+    def test_default(self):
+        input_html = """
+            <html><body>
+                <h1>Heading 1</h1>
+            </body></html>
+            """
+        actual = get_h1_from_html(input_html)
+        expected = "Heading 1"
+        self.assertEqual(actual, expected)
+        self.assertNotIn("<h1>", actual)
+        self.assertNotIn("</h1>", actual)
+
+    def test_missing(self):
+        input_html = """
+            <html><body>
+                <p>html missing heading 1</p>
+            </body></html>
+            """
+        actual = get_h1_from_html(input_html)
+        expected = ""
+        self.assertEqual(actual, expected)
+        self.assertNotIn("<h1>", actual)
+        self.assertNotIn("</h1>", actual)
+
+    def test_empty(self):
+        input_html = """
+            <html><body>
+                <h1></h1>
+            </body></html>
+            """
+        actual = get_h1_from_html(input_html)
+        expected = ""
+        self.assertEqual(actual, expected)
+
+    def test_multiple(self):
+        input_html = """
+            <html><body>
+                <h1>Heading 1</h1>
+                <h1>Heading 2</h1>
+            </body><html>
+            """
+        actual = get_h1_from_html(input_html)
+        expected = "Heading 1"
+        self.assertEqual(actual, expected)
+
+
+class TestGetFirstParagraphFromHTML(unittest.TestCase):
+    def test_default(self):
+        input_html = """
+            <html><body>
+                <h1>Heading 1</h1>
+                <main>
+                    <p>body paragraph</p>
+                </main>
+            </body></html>
+            """
+        actual = get_first_paragraph_from_html(input_html)
+        expected = "body paragraph"
+        self.assertEqual(actual, expected)
+
+    def test_high_priority(self):
+        input_body = """
+            <html><body>
+                <p>low priority</p>
+                <main>
+                    <p>high priority</p>
+                </main>
+            </body></html>
+            """
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "high priority"
+        self.assertEqual(actual, expected)
+        self.assertNotIn("<p>", actual)
+        self.assertNotIn("</p>", actual)
+
+    def test_low_priority(self):
+        input_body = """
+            <html><body>
+                <p>low priority</p>
+                <main>
+                    <h1>missing high priority</h1>
+                </main>
+            </body></html>
+            """
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "low priority"
+        self.assertEqual(actual, expected)
+        self.assertNotIn("<p>", actual)
+        self.assertNotIn("</p>", actual)
+
+    def test_missing_main(self):
+        input_body = """
+            <html><body>
+                <p>low priority</p>
+            </body></html>
+            """
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "low priority"
         self.assertEqual(actual, expected)
 
 
