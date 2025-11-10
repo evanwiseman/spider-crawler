5a12ba822b6cd1ea5b57a94698f2e48de5b215c3
evanwiseman <evanwiseman2000@gmail.com>
2025-11-09 21:48:04 -0800

    add crawl page to recursively get pages

--- crawl.py
+++ crawl.py
@@ -1,13 +1,27 @@
+import requests
 from bs4 import BeautifulSoup
-from urllib import parse
-from typing import List
+from urllib.parse import urlparse, urljoin
+from typing import List, Optional
 
 
 def normalize_url(url: str) -> str:
-    result = parse.urlparse(url)
+    result = urlparse(url)
     joined = result.netloc + result.path
     normalized = joined.rstrip("/").lower()
     return normalized
+
+
+def get_html(url: str) -> str:
+    res = requests.get(url, headers={"User-Agent": "BootCrawler/1.0"})
+
+    if not res.ok:
+        raise Exception("request failed")
+
+    contentType = res.headers.get("content-type")
+    if not contentType or "text/html" not in contentType.lower():
+        raise Exception(f"response content-type invalid: {contentType}")
+
+    return res.text
 
 
 def get_h1_from_html(html: str) -> str:
@@ -40,7 +54,7 @@ def get_urls_from_html(html: str, base_url: str) -> List[str]:
         href = a.get("href")
         if not href:
             continue
-        urls.append(parse.urljoin(base_url, href))
+        urls.append(urljoin(base_url, href))
 
     return urls
 
@@ -54,7 +68,7 @@ def get_images_from_html(html: str, base_url: str) -> List[str]:
         src = img.get("src")
         if not src:
             continue
-        urls.append(parse.urljoin(base_url, src))
+        urls.append(urljoin(base_url, src))
 
     return urls
 
@@ -67,3 +81,49 @@ def extract_page_data(html: str, page_url: str) -> dict:
     data["outgoing_links"] = get_urls_from_html(html, page_url)
     data["image_urls"] = get_images_from_html(html, page_url)
     return data
+
+
+def is_same_domain(url1: str, url2: str) -> bool:
+    parsed1 = urlparse(url1)
+    parsed2 = urlparse(url2)
+    if parsed1.netloc == parsed2.netloc:
+        return True
+    return False
+
+
+def crawl_page(
+    base_url: str,
+    current_url: Optional[str] = None,
+    page_data: Optional[dict] = None,
+) -> dict:
+    if not current_url:
+        current_url = base_url
+    if not page_data:
+        page_data = {}
+
+    # check base and current domain match (skip)
+    if not is_same_domain(base_url, current_url):
+        return
+
+    # normalize the current url
+    normalized_current = normalize_url(current_url)
+
+    # check if the page has already been crawled (skip)
+    if normalized_current in page_data:
+        return
+
+    print(f"extracting from {current_url}...")
+    try:
+        html = get_html(current_url)
+        data = extract_page_data(html, current_url)
+        page_data[normalized_current] = data
+        for url in data["outgoing_links"]:
+            crawl_page(current_url, url, page_data)
+    except Exception:
+        # mark as seen and skip
+        page_data[normalized_current] = None
+        print(f"failed extracting from {current_url}")
+        return page_data
+
+    print(f"finished extracting from {current_url}.")
+    return page_data
--- main.py
+++ main.py
@@ -1,5 +1,44 @@
+import sys
+import textwrap
+from crawl import crawl_page
+
+
 def main():
-    print("Hello from spider-crawler!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    print(f"starting crawl of: {url}")
+
+    page_data = crawl_page(url)
+    for normalized_url, data in page_data.items():
+        # invalid entry
+        if not isinstance(data, dict):
+            continue
+
+        print(normalized_url)
+        for k, v in data.items():
+            if isinstance(v, list):
+                print(f"\t{k}: [")
+                for item in v:
+                    text = textwrap.fill(
+                        str(item),
+                        initial_indent="\t\t",
+                        subsequent_indent="\t\t",
+                    )
+                    print(text)
+                print("\t]")
+            else:
+                text = textwrap.fill(
+                    str(v),
+                    initial_indent="",
+                    subsequent_indent="\t\t",
+                )
+                print(f"\t{k}: {text}")
 
 
 if __name__ == "__main__":
